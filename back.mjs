219ae3e43cce8b160b11851703f49f8d1af7234e
Akinde Dipo <akindedipo@gmail.com>
2024-12-16 22:01:59 +0100

    fix4

--- back.mjs
+++ back.mjs
@@ -1,6 +1,3 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "API is working!" });
-}
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
